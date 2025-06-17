1ed0583073a79c5616b0c1fe25149a153fc1c3a6
pantheonsteve <steve.bresnick@datadoghq.com>
2025-06-17 10:29:22 -0400

    Add Express server for SPA routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   define: {
     'process.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID),
     'process.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
