ced062c4a8b45f56c6acb977e29311c3b2650049
pantheonsteve <steve.bresnick@datadoghq.com>
2025-06-17 10:42:10 -0400

    Fix package.json syntax error and add Express server

--- frontend/server.js
+++ frontend/server.js
@@ -30,7 +30,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
